dc55fd3b367077551cab3cdf8a92e360d31f625c
prethivashok <46440591+PRETHIV@users.noreply.github.com>
2019-09-09 18:26:39 +0530

    Create Just_Run_me.py

---
+++ Just_Run_me.py
@@ -0,0 +1,9 @@
+import os
+os.makedirs("Face Recognition")
+os.chdir("/Face Recognition")
+os.makedirs("Cascades")
+os.makedirs("dataset")
+os.makedirs("trainer")
+os.system("pip install pillow")
+os.system("pip install opencv-contrib-python")
+os.system("pip install pymysql")
